1c603cfa1ef2abdcceb6c1210cae3e5e1a1cb305
Tony <asolomon484@gmail.com>
2023-07-07 11:40:33 +0100

    updated the branch

--- components/Updatefreetip.jsx
+++ components/Updatefreetip.jsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
@@ -21,26 +20,26 @@ export default function Updatefreetips() {
 
   useEffect(() => {
     // Fetch data for the specific tip using the "id" parameter
-    // const fetchData = async () => {
-    //   try {
-    //     const url = `https://tasty-duck-coveralls.cyclic.app/v1/premium/64a5b12293f891c4b8da2db1`;
-    //     const response = await axios.get(url);
-    //     console.log(response.data);
-    //     // const { date, time, league, match, odds, tip, scores } = response.data;
-    //     // setFormData({ date, time, league, match, odds, tip, scores });
-    //   } catch (error) {
-    //     console.error("Error:", error.response);
-    //     setError("Failed to fetch tip data");
-    //   }
-    // };
+    const fetchData = async () => {
+      try {
+        const url = `https://tasty-duck-coveralls.cyclic.app/v1/freetip/${_id}`;
+        const response = await axios.get(url);
+        console.log(response.data);
+        const { date, time, league, match, odds, tip, scores } = response.data;
+        setFormData({ date, time, league, match, odds, tip, scores });
+      } catch (error) {
+        console.error("Error:", error.response);
+        setError("Failed to fetch tip data");
+      }
+    };
 
     if (_id) {
-      // fetchData();
-      console.log(typeof _id)
-      fetch (`https://tasty-duck-coveralls.cyclic.app/v1/premium/${_id.trim()}`)
-      .then(data => data.json())
-      .then(response => console.log(response))
-      .catch(err => console.log(err))
+      fetchData();
+      //   console.log(typeof _id)
+      //   fetch (`https://tasty-duck-coveralls.cyclic.app/v1/premium/${_id.trim()}`)
+      //   .then(data => data.json())
+      //   .then(response => console.log(response))
+      //   .catch(err => console.log(err))
     }
   }, [_id]);
 
@@ -54,7 +53,7 @@ export default function Updatefreetips() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const url = `https://tasty-duck-coveralls.cyclic.app/v1/premium/${_id}`;
+      const url = `https://tasty-duck-coveralls.cyclic.app/v1/freetip/${_id}`;
       const response = await axios.put(url, formData);
       console.log(response.data); // Handle the response as needed
       // Reset form data if needed
@@ -86,93 +85,93 @@ export default function Updatefreetips() {
 
   return (
     <div className="flex justify-center items-center">
-      {/* <div>
+      <div>
         <h1 className="text-center text-3xl">Updatetip</h1>
-        <form className='text-app-black' onSubmit={handleSubmit}>
-
-          <div className='mt-4'>
+        <form className="text-app-black" onSubmit={handleSubmit}>
+          <div className="mt-4">
             <input
-              type='text'
-              name='date'
+              type="text"
+              name="date"
               value={formData.date}
               onChange={handleInputChange}
-              placeholder='date'
-              className='p-3 w-full'
+              placeholder="date"
+              className="p-3 w-full"
             />
           </div>
-          <div className='mt-4'>
+          <div className="mt-4">
             <input
-              type='text'
-              name='time'
-              placeholder='time'
+              type="text"
+              name="time"
+              placeholder="time"
               value={formData.time}
               onChange={handleInputChange}
-              className='p-3'
+              className="p-3"
             />
           </div>
 
-          <div className='mt-4'>
+          <div className="mt-4">
             <input
-              type='text'
-              name='league'
-              placeholder='league'
+              type="text"
+              name="league"
+              placeholder="league"
               value={formData.league}
               onChange={handleInputChange}
-              className='p-3'
+              className="p-3"
             />
           </div>
 
-          <div className='mt-4'>
+          <div className="mt-4">
             <input
-              type='text'
-              name='match'
-              placeholder='match'
+              type="text"
+              name="match"
+              placeholder="match"
               value={formData.match}
               onChange={handleInputChange}
-              className='p-3'
+              className="p-3"
             />
           </div>
 
-          <div className='mt-4'>
+          <div className="mt-4">
             <input
-              type='text'
-              name='odds'
-              placeholder='odds'
+              type="text"
+              name="odds"
+              placeholder="odds"
               value={formData.odds}
               onChange={handleInputChange}
-              className='p-3'
+              className="p-3"
             />
           </div>
 
-          <div className='mt-4'>
+          <div className="mt-4">
             <input
-              type='text'
-              name='tip'
-              placeholder='tip'
+              type="text"
+              name="tip"
+              placeholder="tip"
               value={formData.tip}
               onChange={handleInputChange}
-              className='p-3'
+              className="p-3"
             />
           </div>
 
-          <div className='mt-4'>
+          <div className="mt-4">
             <input
-              type='text'
-              name='scores'
-              placeholder='scores'
+              type="text"
+              name="scores"
+              placeholder="scores"
               value={formData.scores}
               onChange={handleInputChange}
-              className='p-3'
+              className="p-3"
             />
           </div>
 
-          <button className='mt-4 h-[2.25rem] w-[6.8rem] grid place-items-center bg-gradient-to-r from-app-orange via-app-sky to-app-orange p-[1px] rounded-lg cursor-pointer hover:p-[2px] text-white'>
-            <span className='bg-app-black w-full h-full p-[1px] text-sm rounded-lg inline-grid place-items-center'>Update</span>
+          <button className="mt-4 h-[2.25rem] w-[6.8rem] grid place-items-center bg-gradient-to-r from-app-orange via-app-sky to-app-orange p-[1px] rounded-lg cursor-pointer hover:p-[2px] text-white">
+            <span className="bg-app-black w-full h-full p-[1px] text-sm rounded-lg inline-grid place-items-center">
+              Update
+            </span>
           </button>
         </form>
         {error && <p className="bg-red-500">{error}</p>}
-
-      </div> */}
+      </div>
     </div>
   );
 }
